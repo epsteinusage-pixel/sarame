1d696aaa697a806ba24c9ec0d69314df33b6c7ae
epsteinusage-pixel <257419119+epsteinusage-pixel@users.noreply.github.com>
2026-03-07 16:41:18 -0800

    Implement Discord bot with Flask API for messages

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from flask import Flask, jsonify
+import threading
+import os
+
+app = Flask(__name__)
+storage = {"msg": ""}
+
+@app.route('/get_message', methods=['GET'])
+def get_message():
+    return jsonify({"content": storage["msg"]})
+
+@app.route('/')
+def health():
+    return "Online"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+TOKEN = 'MTQ3OTk4ODI0NzI5NDY0MDI1OQ.GkaNKy.mJqt6X9vJSY50NP6PNLTR5IK3dqWs1c0uJYkIc'
+CHAN_ID = 1479671475643617280
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user or message.channel.id != CHAN_ID:
+        return
+    storage["msg"] = message.content
+    await message.channel.send(message.content)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask, daemon=True).start()
+    bot.run(TOKEN)
